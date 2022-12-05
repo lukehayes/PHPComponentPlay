55f87035524ef7e77c6aecb523d3818bc9a8fe29
Luke Hayes <lukepurplehayes@gmail.com>
2022-12-05 12:18:55 +0000

    Added basic router.

--- public/index.php
+++ public/index.php
@@ -2,4 +2,24 @@
 
 // Start of project.
 
+require '../vendor/autoload.php';
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request = Request::createFromGlobals();
+$server = $request->server;
+$path = $request->getPathInfo();
+
+// First iteration of a router/front-controller.
+
+if(in_array($path, ['/']))
+{
+    $response = new Response('Showing the homepage.');
+}else
+{
+    $response = new Response('404. Something has gone wrong');
+}
+
+dump($response);
 
