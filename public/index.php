022c37820b56cd52b69ed28b0affae3c0f7e5c68
lukehayes <lukepurplehayes@gmail.com>
2022-12-05 18:17:52 +0000

    Added view() helper function to dislay templates.

--- public/index.php
+++ public/index.php
@@ -11,14 +11,21 @@ $request = Request::createFromGlobals();
 $server  = $request->server;
 $path    = $request->getPathInfo();
 
-// First iteration of a router/front-controller.
+/**
+ * Get the contents of a template
+ */
+function view(string $file): string
+{
+    return file_get_contents("../templates/$file.php");
+}
 
+// First iteration of a router/front-controller.
 if (in_array($path, ['/'])) {
     $response = new Response('Homepage.');
 } else if (in_array($path, ['/contact'])) {
-    $response = new Response('Contact Page');
+    $response = new Response(view('form'));
 } else {
-    $response = new Response('404. Something has gone wrong');
+    $response = new Response(view('error'));
 }
 
 // Output page to browser.
