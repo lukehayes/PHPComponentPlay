12b4323158fb3730f3eb6006fa215063a6371c26
lukehayes <lukepurplehayes@gmail.com>
2022-12-05 17:26:52 +0000

    Added extra route for /contact.

--- public/index.php
+++ public/index.php
@@ -8,18 +8,18 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 $request = Request::createFromGlobals();
-$server = $request->server;
-$path = $request->getPathInfo();
+$server  = $request->server;
+$path    = $request->getPathInfo();
 
 // First iteration of a router/front-controller.
 
-if(in_array($path, ['/']))
-{
-    $response = new Response('Showing the homepage.');
-}else
-{
+if (in_array($path, ['/'])) {
+    $response = new Response('Homepage.');
+} else if (in_array($path, ['/contact'])) {
+    $response = new Response('Contact Page');
+} else {
     $response = new Response('404. Something has gone wrong');
 }
 
-dump($response);
-
+// Output page to browser.
+$response->send();
