b8bbb3a4a49f4b56523213c8c3f1f1657e32bea3
Luke Hayes <lukepurplehayes@gmail.com>
2022-12-06 10:47:30 +0000

    Created header and footer partials.

--- templates/form.php
+++ templates/form.php
@@ -1,20 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-  <title></title>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="" rel="stylesheet">
-</head>
-
-<body>
-
   <form action="/contact" method="POST">
     <input type="text" name="data" value="" placeholder="Enter a value...">
     <input type="submit" name="" value="Submit">
   </form>
-
-</body>
-
-</html>
