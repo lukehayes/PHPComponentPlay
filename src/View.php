7a0c67b33a07892fb9980af51b5450bda981ca53
Luke Hayes <lukepurplehayes@gmail.com>
2022-12-06 10:47:15 +0000

    Added View::load() helper method.

--- src/View.php
+++ src/View.php
@@ -9,6 +9,14 @@ use Symfony\Component\HttpFoundation\Response;
 class View
 {
     /**
+     * Path to templates directory. **/
+    const TEMPLATE_PATH = "../templates/";
+
+    /**
+     * Path to partials directory. **/
+    const PARTIALS_PATH = self::TEMPLATE_PATH . "/partials/";
+
+    /**
      * Read and return the contents of a template inside an
      * HttpFoundation\Response object.
      *
@@ -19,5 +27,21 @@ class View
     static public function render(string $file) : Response
     {
         return new Response(file_get_contents("../templates/$file.php"));
+    }
+
+    /**
+     * Load a template file with header and footer patials.
+     *
+     * @param string $file    The name of the file to load.
+     *
+     * @return Response
+     */
+    public static function load(string $file) : Response
+    {
+        $template  = file_get_contents("../templates/partials/header.php");
+        $template .= file_get_contents("../templates/$file.php");
+        $template .= file_get_contents("../templates/partials/footer.php");
+
+        return new Response($template);
     }
 }
