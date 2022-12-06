96b9107a8e211a56a967ee2e327c058954eac0c7
Luke Hayes <lukepurplehayes@gmail.com>
2022-12-06 09:47:26 +0000

    Added App\App class to project.

---
+++ src/App.php
@@ -0,0 +1,18 @@
+<?php 
+namespace App;
+
+
+class App
+{
+
+    public function __construct()
+    {
+    }
+
+
+    public function run()
+    {
+        // TODO Implmement entry point.
+    }
+}
+
