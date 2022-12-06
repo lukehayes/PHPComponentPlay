5432a3da454009db144770db805303c26425c5e9
Luke Hayes <lukepurplehayes@gmail.com>
2022-12-06 10:12:57 +0000

    Added missing action and method attributes to test form.

--- src/View.php
+++ src/View.php
@@ -8,6 +8,14 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class View
 {
+    /**
+     * Read and return the contents of a template inside an
+     * HttpFoundation\Response object.
+     *
+     * @param string $file    The name of the file to load.
+     *
+     * @return Response;
+     */
     static public function render(string $file) : Response
     {
         return new Response(file_get_contents("../templates/$file.php"));
--- templates/form.php
+++ templates/form.php
@@ -10,7 +10,7 @@
 
 <body>
 
-  <form>
+  <form action="/contact" method="POST">
     <input type="text" name="data" value="" placeholder="Enter a value...">
     <input type="submit" name="" value="Submit">
   </form>
