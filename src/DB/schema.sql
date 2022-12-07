539b09cbc4be692b191ffc279b72f402e959facf
lukehayes <lukepurplehayes@gmail.com>
2022-12-07 17:41:43 +0000

    Added schema.sql file.

---
+++ src/DB/schema.sql
@@ -0,0 +1,19 @@
+drop table if exists data;
+
+create table data(id integer PRIMARY KEY autoincrement, value integer);
+
+
+insert into data(id, value) values(null, "Value example");
+insert into data(id, value) values(null, "Value example");
+insert into data(id, value) values(null, "Value example");
+insert into data(id, value) values(null, "Value example");
+insert into data(id, value) values(null, "Value example");
+insert into data(id, value) values(null, "Value example");
+insert into data(id, value) values(null, "Value example");
+insert into data(id, value) values(null, "Value example");
+insert into data(id, value) values(null, "Value example");
+insert into data(id, value) values(null, "Value example");
+insert into data(id, value) values(null, "Value example");
+insert into data(id, value) values(null, "Value example");
+insert into data(id, value) values(null, "Value example");
+
