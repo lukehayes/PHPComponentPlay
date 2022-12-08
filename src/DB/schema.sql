e3eaecb3e66adf639aee9a181a2ff047424ce20f
Luke Hayes <lukepurplehayes@gmail.com>
2022-12-08 09:53:18 +0000

    Added quick user table to schema.sql

--- src/DB/schema.sql
+++ src/DB/schema.sql
@@ -2,18 +2,18 @@ drop table if exists data;
 
 create table data(id integer PRIMARY KEY autoincrement, value integer);
 
-
-insert into data(id, value) values(null, "Value example");
-insert into data(id, value) values(null, "Value example");
-insert into data(id, value) values(null, "Value example");
-insert into data(id, value) values(null, "Value example");
-insert into data(id, value) values(null, "Value example");
-insert into data(id, value) values(null, "Value example");
-insert into data(id, value) values(null, "Value example");
-insert into data(id, value) values(null, "Value example");
-insert into data(id, value) values(null, "Value example");
-insert into data(id, value) values(null, "Value example");
-insert into data(id, value) values(null, "Value example");
 insert into data(id, value) values(null, "Value example");
 insert into data(id, value) values(null, "Value example");
 
+drop table if exists users;
+
+create table users(
+    id integer PRIMARY KEY autoincrement,
+    username text,
+    password text
+);
+
+insert into users
+            (id, username, password)
+       values
+            (null, "developer", "password");
