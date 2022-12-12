7a0c581de4446c2ba3d91d94148f1a35c1ce38fc
Luke Hayes <lukepurplehayes@gmail.com>
2022-12-12 12:22:38 +0000

    Added start of very messy and horrible authentication.

--- public/index.php
+++ public/index.php
@@ -16,11 +16,13 @@ $request = Request::createFromGlobals();
 $server  = $request->server;
 $path    = $request->getPathInfo();
 $query = new Query(new SQLiteDatabase("db"));
+$user = NULL;
 
-$sesh = new Session();
-$sesh->getFlashBag()->add('notice', 'A quick flash message');
-$sesh->getFlashBag()->add('login_failed', 'Failed to login.');
-$sesh->getFlashBag()->add('user_not_found', 'Username could not be found');
+$session = new Session();
+//$session->getFlashBag()->add('notice', 'A quick flash message');
+//$session->getFlashBag()->add('login_failed', 'Failed to login.');
+//$session->getFlashBag()->add('user_not_found', 'Username could not be found');
+//$session->getFlashBag()->add('force_login',    'You must sign in to access that page');
 
 // First iteration of a router/front-controller.
 if (in_array($path, ['/'])) {
@@ -29,33 +31,50 @@ if (in_array($path, ['/'])) {
 
     if( $request->getMethod() == "GET")
     {
-        //dump($sesh->getFlashBag()->get('notice'));
+        if( $session->getFlashBag()->has('user_not_found'))
+        {
+            dump($session->getFlashBag()->all());
+        }
+
         // Load the login form if its a GET request.
         $response = View::load('form');
     }else
     {
         // Process the input if POST.
         $username = $request->request->get('username');
-        $user = $query->getUser($username);
+        $password = $request->request->get('password');
+        $user = $query->getUser($username) ?? false;
 
+        // TODO Implement authentication
+        //
+        // Get user input.
+        // Check in database for user.
+        // Check password.
+        // If any failiure, take back to the login screen with errors.
+        // Else - Take user to dashboard, fill session variable with username etc.
+    
+        $password_fail = false;
+        $username_fail = false;
 
-
-        if($user->authenticated)
+        if(!empty($password) && $user->password === $password)
         {
-            echo $sesh->get('login_failed');
             $response = new RedirectResponse('/dashboard');
-        }else
-        {
-            dd($sesh->get('login_failed'));
+        }else {
+            $session->getFlashBag()->add('user_not_found', 'User could not be found');
             $response = new RedirectResponse('/login');
         }
-
-        // If login successful then redirect to the dashboard page.
-        $response = new RedirectResponse('/dashboard');
     }
 
 } else if (in_array($path, ['/dashboard'])) {
-    $response = new Response('Showing Dashboard.');
+    
+    if(!$user)
+    {
+        $session->getFlashBag()->add('user_null', 'User is null');
+        $response = new RedirectResponse('/login');
+    }else
+    {
+        $response = new Response('Showing Dashboard.');
+    }
 } else {
     $response = View::load('error');
 }
--- templates/form.php
+++ templates/form.php
@@ -1,4 +1,5 @@
   <form action="/login" method="POST">
     <input type="text" name="username" value="" placeholder="Username">
+    <input type="text" name="password" value="" placeholder="Password">
     <input type="submit" name="" value="Submit">
   </form>
