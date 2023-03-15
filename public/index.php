1ba1018926ca0e866b2073b7b98953aee0948ea3
Luke Hayes <lukepurplehayes@gmail.com>
2023-03-15 09:48:12 +0000

    Refactored and simplified index.php to remove most of the test code.

--- public/index.php
+++ public/index.php
@@ -4,48 +4,36 @@
 
 require '../vendor/autoload.php';
 
-use App\View;
-// use App\DB\Query;
-// use App\DB\SQLiteDatabase;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-// use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpFoundation\Session\Session;
+// use Symfony\Component\HttpFoundation\Session\Session;
 
-use App\Route;
 use App\Controllers\TestController;
 
-$routes = [
-    new Route("/", "Site", "index"),
-    new Route("/other", "Site", "other"),
-];
+$request    = Request::createFromGlobals();
+$controller = new TestController();
+$path       = $request->getPathInfo();
+$method     = $request->server->get('REQUEST_METHOD');
 
+// Routing -----------------------------------------
+// -------------------------------------------------
 
-$tc = new TestController();
-
-
-
-$request = Request::createFromGlobals();
-$server  = $request->server;
-$path    = $request->getPathInfo();
-
-$session = new Session();
-
-switch($path)
+if($method === "GET")
 {
-    case "/":
-        // $response = new Response("/");
-        $response = $tc->index($request);
-        break;
-    case "/other":
-        $response = $tc->other($request);
-        break;
-    default:
-        $response = new Response("Error, 404.", 404);
-        break;
+    switch($path)
+    {
+        case "/":
+            $controller->index()->send();
+            break;
+        case "/login":
+            $controller->other()->send();
+            break;
+        default:
+            $response = new Response("Error, 404.", 404);
+            $response->send();
+            break;
+    }
+}else 
+{
+    dd("Revieved POST request. Dying.");
 }
-
-$response->send();
-
-
-
