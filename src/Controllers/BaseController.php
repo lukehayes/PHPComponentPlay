307a810758c86bfa9742dbd2a510e4670950f571
lukehayes <lukepurplehayes@gmail.com>
2023-03-15 15:08:25 +0000

    App\Controllers\BaseController is now using App\Request class.

--- src/Controllers/BaseController.php
+++ src/Controllers/BaseController.php
@@ -1,14 +1,14 @@
 <?php
 namespace App\Controllers;
 
-use Symfony\Component\HttpFoundation\Request;
+use App\Request;
 
 class BaseController
 {
-  public $request = NULL;
+    public $request = NULL;
 
-  public function __construct()
-  {
-    $this->request = Request::createFromGlobals();
-  }
+    public function __construct()
+    {
+        $this->request = new Request();
+    }
 }
