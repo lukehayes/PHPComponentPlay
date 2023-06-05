da0eb28fd04b26bd21a4ee047e6cf46a8600e223
Luke Hayes <lukepurplehayes@gmail.com>
2023-06-05 11:37:12 +0100

    Added App\Routing\Router class to project.

---
+++ src/Routing/Router.php
@@ -0,0 +1,59 @@
+<?php 
+namespace App\Routing;
+
+use App\Routing\Route;
+
+
+class Router
+{
+    /**
+     * A multi dimensional array for all of the routes
+     * available to the application.
+     */
+    private array|null $routes = null;
+
+    public function __construct()
+    {
+        $this->setDefaultRoutes();
+    }
+
+    /**
+     * Set the array of available default routes.
+     * Called internally.
+     *
+     * @return void.
+     */
+    private function setDefaultRoutes()
+    {
+        $this->routes['GET'] = [];
+        $this->routes['POST'] = [];
+    }
+
+    /**
+     * Routes getter function.
+     *
+     * @return array
+     */
+    public function getRoutes() : array
+    {
+        return $this->routes;
+    }
+
+    /**
+     * Add a GET route to the router. If the key already
+     * exists, then false is returned.
+     *
+     * @param Route $route.
+     *
+     * @return bool.
+     */
+    public function get(Route $route) : bool
+    {
+        if(array_key_exists($route->getName(), $this->routes))
+            return false;
+
+        $this->routes['GET'][$route->getName()] = $route;
+        return true;
+    }
+
+}
