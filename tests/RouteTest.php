5d1e80af66d0efcb3ec60edb125525af786fbbbb
lukehayes <lukepurplehayes@gmail.com>
2023-06-05 15:27:59 +0100

    Added tests/RouteTest to project.

---
+++ tests/RouteTest.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+use App\Routing\Route;
+
+final class RouteTest  extends TestCase
+{
+    public $route = NULL;
+
+    public function setup() : void
+    {
+        $this->route = new Route('home','/', 'TestController', 'TestAction');
+    }
+
+    public function testRouteGetters(): void
+    {
+        $this->assertEquals($this->route->getName(), 'home');
+        $this->assertEquals($this->route->getPath(), '/');
+        $this->assertEquals($this->route->getController(), 'TestController');
+        $this->assertEquals($this->route->getAction(), 'TestAction');
+    }
+
+}
