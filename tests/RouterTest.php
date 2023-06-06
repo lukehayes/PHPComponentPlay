a76dd083c797e1c7b59561628785f6a239333093
Luke Hayes <lukepurplehayes@gmail.com>
2023-06-06 10:21:08 +0100

    Refactored tests to add an instance of Route to constructor.

--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -8,28 +8,42 @@ final class RouterTest  extends TestCase
 {
     public $router = NULL;
 
+    public $testRoute = NULL;
+
+
     public function setup() : void
     {
         $this->router = new Router();
+        $this->testRoute = new Route('home', '/', 'TestController', 'TestAction');
+
+        $this->router->get($this->testRoute);
+        dump($this->router);
     }
 
     public function testCanAddGetRoute()
     {
-        $route = new Route('home', '/', 'TestController', 'TestAction');
+        $newRoute = new Route('other', '/other', 'OtherController', 'OtherAction');
 
-        $this->assertTrue($this->router->get($route));
+        // Route already added, so this should fail.
+        $this->assertFalse($this->router->get($this->testRoute));
 
-        $this->assertArrayHasKey($route->getName(), $this->router->getRoutes()['GET']);
+        // Route NOT already added, so this should pass.
+        $this->assertTrue($this->router->get($newRoute));
+
+        $this->assertArrayHasKey($newRoute->getPath(), $this->router->getRoutes()['GET']);
+
+        $this->assertArrayHasKey($newRoute->getPath(), $this->router->getRoutes()['GET']);
+    }
+
+    public function testCanFindRoute()
+    {
+        $this->assertTrue($this->router->findRoute());
     }
 
     public function testNoDuplicateGetRoute()
     {
-        $route = new Route('home', '/', 'TestController', 'TestAction');
+        $this->assertFalse($this->router->get($this->testRoute));
 
-        $this->assertTrue($this->router->get($route));
-
-        $this->assertArrayHasKey($route->getName(), $this->router->getRoutes()['GET']);
-
-        $this->assertFalse($this->router->get($route));
+        $this->assertArrayHasKey($this->testRoute->getPath(), $this->router->getRoutes()['GET']);
     }
 }
