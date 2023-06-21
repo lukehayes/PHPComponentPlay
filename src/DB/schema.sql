d80b023c168c9b821a87dca0e39b11769410b30e
Luke Hayes <lukepurplehayes@gmail.com>
2023-06-21 09:42:32 +0100

    Corrected schema double quotes to single quotes.

--- src/DB/schema.sql
+++ src/DB/schema.sql
@@ -2,8 +2,8 @@ drop table if exists data;
 
 create table data(id integer PRIMARY KEY autoincrement, value integer);
 
-insert into data(id, value) values(null, "Value example");
-insert into data(id, value) values(null, "Value example");
+insert into data(id, value) values(null, 'Value example');
+insert into data(id, value) values(null, 'Value example');
 
 drop table if exists users;
 
@@ -15,5 +15,5 @@ create table users(
 
 insert into users (id, username, password)
        values
-            (null, "developer", "password"),
-            (null, "admin", "password");
+            (null, 'developer', 'password'),
+            (null, 'admin', 'password');
