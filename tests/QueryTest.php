489ba7e1b3faef3db7a2ae5d0f80fd5891de21c6
Luke Hayes <lukepurplehayes@gmail.com>
2023-06-21 09:56:02 +0100

    Added start of Query unit test.

---
+++ tests/QueryTest.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+use App\App;
+use App\Container;
+use App\DB\Query;
+use App\DB\Database;
+
+/**
+ * Tests for \App\DB\Query class, not the class that will use
+ * the Doctrine DBAL library.
+ */
+final class QueryTest extends TestCase
+{
+    public $app;
+    public $container;
+    public $query;
+    public $db;
+
+    public function setup() : void
+    {
+        $this->app = new App();
+        $this->container = new Container();
+        $this->app::setContainer($this->container);
+
+        $this->db = $this->container->getInstance(\App\Service\DatabaseService::class);
+        $this->query = new Query($this->db->get());
+    }
+
+    public function testCanSelectAll()
+    {
+        $rows = $this->query->selectAll('users');
+
+        $this->assertIsArray($rows);
+    }
+
+    public function testSelectAllNotEmpty()
+    {
+        $rows = $this->query->selectAll('users');
+        $this->assertGreaterThan(0, count($rows));
+    }
+}
