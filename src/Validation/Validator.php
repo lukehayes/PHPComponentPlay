9f6d485dce69c50b206371bb1b3886aa43338da9
lukehayes <lukepurplehayes@gmail.com>
2024-07-29 12:00:48 +0100

    Added App\Validation\Validator class

---
+++ src/Validation/Validator.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Validation;
+
+class Validator
+{
+    /*
+     * Run validations on a string.
+     *
+     * @param string $str    The string to validate.
+     *
+     * @return string    The validated string.
+     */
+    public static function validate(string $str) : string
+    {
+        return self::sanitize($str);
+    }
+
+    /*
+     * Sanitize a string using filter_var and htmlspecialchars.
+     *
+     * @param string $str    The string to sanitize.
+     *
+     * @return string    The sanitized string.
+     */
+    private static function sanitize(string $str) : string
+    {
+        $str = filter_var($str,
+                            FILTER_SANITIZE_URL |
+                            FILTER_DEFAULT);
+
+        $str = htmlspecialchars($str);
+
+        return $str;
+    }
+
+
+}
